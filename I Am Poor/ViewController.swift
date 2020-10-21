72bdbff913700d4964739316180d2a478c1efd6c
Luiz Doreto <luiz.doreto@icloud.com>
2020-10-21 14:06:17 -0300

    Initial Commit

---
+++ I Am Poor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by Luiz Doreto on 21/10/20.
+//  Copyright © 2020 Doreto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
